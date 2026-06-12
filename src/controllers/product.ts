ad05507f8ed95f12355f6496f01640304b423dd1
fevico <ajayifavour81@gmail.com>
2026-06-12 09:30:41 +0100

    product listing and filter by category

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -4,6 +4,8 @@ import { isValidObjectId } from "mongoose";
 import cloudUploader, { cloudApi } from "src/cloud";
 import ProductModel from "src/models/product";
 import { sendErrorRes } from "src/utils/helper";
+import { UserDocument } from "src/models/user";
+import categories from "src/utils/categories";
 
 const uploadImage = (filePath: string): Promise<UploadApiResponse> => {
     return cloudUploader.upload(filePath, {
@@ -83,7 +85,8 @@ export const updateProduct: RequestHandler = async (req, res) => {
         const isMultipleImages = Array.isArray(images)
 
         if(isMultipleImages){
-            if(product.images.length + images.length > 5){
+            const oldImages = product.images?.length || 0;
+            if(oldImages + images.length > 5){
                 return sendErrorRes(res, "You can upload a maximum of 5 images.", 400);
             }
         }
@@ -116,11 +119,14 @@ export const updateProduct: RequestHandler = async (req, res) => {
             const newImages = await uploadResult.map(({secure_url, public_id}) =>{
                 return {url: secure_url, id: public_id}
             })
-            product.images.push(...newImages);
+            if(product.images) product.images.push(...newImages);
+            else product.images = newImages; 
         }else{
             if(images){
                const {secure_url, public_id} = await uploadImage(images.filepath) 
+               if(product.images)
                product.images.push({url: secure_url, id: public_id})
+                else product.images = [{url: secure_url, id: public_id}]
             }
         }
         await product.save();
@@ -139,10 +145,108 @@ export const deleteProduct: RequestHandler = async (req, res) => {
    const product = await ProductModel.findOneAndDelete({_id:productId, owner: req.user.id})
    if(!product) return sendErrorRes(res, "Product not found!", 404)
 //    remove iimage from cloud
-const images = product.images
+    const images = product.images || []
     if(images.length){
        const ids = images.map(({id}) => id)
         await cloudApi.delete_resources(ids)
     }
      res.status(201).json({message: "Product deleted successfully"})
+}
+
+export const deleteProductImage: RequestHandler = async (req, res) => {
+    const {productId, imageId} = req.params;
+    if(isValidObjectId(productId)){
+        return sendErrorRes(res, "Invalid product id", 422);
+    }
+    const product = await ProductModel.findOneAndUpdate({_id: productId, owner: req.user.id}, {
+        $pull: {images: {id: imageId}}
+    }, {new: true})
+    if(!product) return sendErrorRes(res, "Product not found!", 404)
+    if(product.thumbnail?.includes(imageId)){
+        const images = product.images
+        if(images)
+        product.thumbnail = images[0].url;
+        else product.thumbnail = ""
+        await product.save();
+    }
+    await cloudUploader.destroy(imageId)
+    res.status(200).json({message: "Image removed successfully"})
+}
+
+export const getProductDetail: RequestHandler = async (req, res) => {
+    const {id} = req.params;
+    if(isValidObjectId(id)){
+        return sendErrorRes(res, "Invalid product id", 422);
+    }
+    const product = await ProductModel.findById(id).populate<{owner: UserDocument}>("owner")
+    if(!product) return sendErrorRes(res, "Product not found!", 404)
+    res.status(200).json({message: "Product detail retrieved successfully", product: {
+        id: product._id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        category: product.category,
+        purchasingdate: product.purchasingDate,
+        images: product.images?.map(img => img.url) || [],
+        thumbnail: product.thumbnail,
+        seller: {
+            id: product.owner._id,
+            name: product.owner.name,
+            avatar: product.owner.avatar?.url
+        }
+    }})
+}
+
+export const getProductByCategory: RequestHandler = async (req, res) => {
+    const {category} = req.params;
+    const {pageNo='1', limit='10'} = req.query as {pageNo: string, limit: string};
+    if(!categories.includes(category)) return sendErrorRes(res, "Invalid category", 422);
+    const products = await ProductModel.find({category}).sort('-createdAt').skip((+pageNo - 1) * +limit).limit(+limit);
+   const listings = products.map(p => {
+        return {
+            id: p._id,
+            name: p.name,
+            thumbnail: p.thumbnail,
+            category: p.category,
+            price: p.price,
+        }
+    })
+    res.json({products: listings})
+}
+
+export const getLatestProduct: RequestHandler = async (req, res) => {
+    const products = await ProductModel.find().sort('-createdAt').limit(10)
+   const listings = products.map(p => {
+        return {
+            id: p._id,
+            name: p.name,
+            thumbnail: p.thumbnail,
+            category: p.category,
+            price: p.price,
+        }
+    })
+    res.json({products: listings})
+}
+
+export const getListings: RequestHandler = async (req, res) => {
+    const {pageNo='1', limit='10'} = req.query as {pageNo: string, limit: string};
+    const products = await ProductModel.find({owner: req.user.id}).sort('-createdAt').skip((+pageNo - 1) * +limit).limit(+limit);
+   const listings = products.map(p => {
+        return {
+            id: p._id,
+            name: p.name,
+            thumbnail: p.thumbnail,
+            category: p.category,
+            price: p.price,
+            images: p.images?.map(i=> i.url),
+            description:p.description,
+            date: p.purchasingDate,
+            seller: {
+                id: req.user.id,
+                name: req.user.name,
+                avatar: req.user.avatar
+            }
+        }
+    })
+    res.json({products: listings})
 }
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteProduct, listProduct, updateProduct } from "src/controllers/product";
+import { deleteProduct, deleteProductImage, getLatestProduct, getListings, getProductByCategory, getProductDetail, listProduct, updateProduct } from "src/controllers/product";
 import { isAuth } from "src/middleware/auth";
 import fileParser from "src/middleware/fileParser";
 import validate from "src/middleware/validator";
@@ -10,5 +10,10 @@ const productRouter = Router()
 productRouter.post("/list", isAuth, fileParser, validate(newProductSchema), listProduct)
 productRouter.patch("/:id", isAuth, fileParser, validate(newProductSchema), updateProduct)
 productRouter.delete("/:id", isAuth, deleteProduct)
+productRouter.delete("/image/:product/:imageId", isAuth, deleteProductImage)
+productRouter.get("/detail/:id", isAuth, getProductDetail) 
+productRouter.get("/by-category/:category", isAuth, getProductByCategory) 
+productRouter.get("/latest", getLatestProduct) 
+productRouter.get("/listings", isAuth, getListings) 
 
 export default productRouter;
